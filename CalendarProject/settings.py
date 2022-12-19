531a9aee122f7f1a493d2ee286e78d7cb7bda0a2
Giorgi Maisuradze <107479420+Giorma@users.noreply.github.com>
2022-12-19 18:59:21 +0400

    Update settings.py

--- CalendarProject/settings.py
+++ CalendarProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)tie!a#i#fpn_+p##gt)+*-j!z*epk_o=ke*ki)1f*&h!^b8j8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 APPS_DIR=True
 
